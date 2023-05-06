a7456e7f0be5a5250e535e1dc730ef81362ffc16
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-05-06 09:42:41 +0300

    update

--- src/iconRender.js
+++ src/iconRender.js
@@ -1,48 +1,59 @@
+import imageOne from './images/image-1.png';
+import imageTwo from './images/image-2.png';
+import imageThree from './images/image-3.png';
+import imageFour from './images/image-4.png';
+import imageFive from './images/image-5.png';
+import imageSix from './images/image-6.png';
+import imageSeven from './images/image-7.png';
+import imageEight from './images/image-8.png';
+import imageNine from './images/image-9.png';
+
+
 export const iconRender = [
     {
       title: 'Save the Children',
       url:
         'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-      img: './im',
+      img: imageOne,
     },
     {
       title: 'Project HOPE',
       url: 'https://www.projecthope.org/country/ukraine/',
-      img: null,
+      img: imageTwo,
     },
     {
       title: 'UNITED24',
       url: 'https://u24.gov.ua/uk',
-      img: null,
+      img: imageThree,
     },
     {
       title: 'International Medical Corps',
       url: 'https://internationalmedicalcorps.org/country/ukraine/',
-      img: null,
+      img: imageFour,
     },
     {
       title: 'Medicins Sans Frontieres',
       url: 'https://www.msf.org/ukraine',
-      img: null,
+      img: imageFive,
     },
     {
       title: 'RAZOM',
       url: 'https://www.razomforukraine.org/',
-      img: null,
+      img: imageSix,
     },
     {
       title: 'Action against hunger',
       url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-      img: null,
+      img: imageSeven,
     },
     {
       title: 'World vision',
       url: 'https://www.wvi.org/emergencies/ukraine',
-      img: null,
+      img: imageEight,
     },
     {
       title: 'Serhiy Prytula Charity Foundation',
       url: 'https://prytulafoundation.org/en',
-      img: null,
+      img: imageNine,
     },
   ]
--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
-
+import "./css/styles.css"
 import { iconRender } from './iconRender';
+import Swiper from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+
 
 
 const listIcon = document.querySelector('.list-icon');
+const icomList = document.querySelector('.icon-list');
+const swiperScroll = document.querySelector('.swiper-scrollbar');
+const swiperContainer = document.querySelector('.swiper');
 const itemsMarkup = rendarListIcon(iconRender);
 listIcon.insertAdjacentHTML('beforeend', itemsMarkup);
 
+
 function rendarListIcon (iconRender) {
-      return iconRender.map(({title, url, img}) => {
+      return iconRender.map(({ url, img }) => {
      return `
-        <li>
-        <a class="" href="${url}">
-        <img class="" src="${img}">
+        <li class='icon-list swiper-slide'>  
+        <a class="sl" href="${url}">
+        
+        <img class="fl" src="${img}">
         </a>
         </li>
-        
         `
 }).join('');
-   //  listIcon.insertAdjacentHTML('beforeend', iconRender.join(''));
 }
+{/* <p>${index < 11 ? '0' + (index + 1) : index}</p>  */}
+ 
+
+new Swiper(swiperContainer, {
+   direction: 'vertical',
+   loop: true,
+   modules: [Navigation, Pagination],
+   slidesPerView: 4, // показывать по 3 превью
+   spaceBetween: 20, // расстояние между слайдами
+   freeMode: true, // при перетаскивании превью ведет себя как при скролле
+   loop: true,
+   navigation: {
+      nextEl: '.swiper-button-next',
+    },
+   
+});
+
+
+
+
