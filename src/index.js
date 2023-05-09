cc11a7df636558be96fa8511a25e59c1a60f7959
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-05-09 18:40:05 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import "./css/styles.css"
+import './css/styles.css';
 import { iconRender } from './iconRender';
 import Swiper from 'swiper';
 import Swiper, { Navigation, Pagination } from 'swiper';
-
-
 
 const listIcon = document.querySelector('.list-icon');
 const icomList = document.querySelector('.icon-list');
@@ -12,52 +10,48 @@ const swiperContainer = document.querySelector('.swiper');
 const itemsMarkup = rendarListIcon(iconRender);
 listIcon.insertAdjacentHTML('beforeend', itemsMarkup);
 
-
-function rendarListIcon (iconRender) {
-      return iconRender.map(({title, url, img }) => {
-         
-     return `
+function rendarListIcon(iconRender) {
+  return iconRender
+    .map(({ title, url, img, img2 }) => {
+      return `
         <li class='icon-list swiper-slide'> 
         <p class="title">${title}</p>
         <a class="sl" href="${url}" target="_blank">
-        <img class="fl image-slider" src="${img}">
+        <img class="fl image-slider" src="${img, img2}">
         </a>
         </li>
-        `
-}).join('');
+        `;
+    })
+    .join('');
 }
-   
- 
 
 new Swiper(swiperContainer, {
-   spaceBetween: 20, // расстояние между слайдами
-   direction: 'vertical',
-   modules: [Navigation, Pagination],
-   navigation: {
-      nextEl: '.swiper-button-next',
-    },
+  spaceBetween: 20, // расстояние между слайдами
+  direction: 'vertical',
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
 
-    mousewheel: {
-      sensitivity: 1,
-    },
+  mousewheel: {
+    sensitivity: 1,
+  },
 
-    breakpoints: {
-      320: {
-         slidesPerView: 4, //показывать по 4 превью
-         loop: true,
-       },
-       768: {
-         slidesPerView: 6, //показывать по 6 превью
-         loop: true,
-       }
+  rewind: true,
+  slidesPerView: 3,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 4, //показывать по 4 превью
     },
-   //  loop: true, // бесконечная прокрутка
-   // scrollbar: {
-   //    el: '.swiper-scrollbar',
-   //    draggable: true,
-   //  },
-   
- 
+    768: {
+      slidesPerView: 6, //показывать по 6 превью
+    },
+  },
+
+  scrollbar: {
+    el: swiperScroll,
+  },
 });
 
 
