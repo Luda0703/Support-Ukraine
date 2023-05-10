060e7f0ed4f05a823b2e60f53c2ccc8cdb00070e
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-05-10 17:37:44 +0300

    update

--- src/scrollup.js
+++ src/scrollup.js
@@ -1,14 +1,27 @@
- const loadMoreBtn = document.querySelector(".scrollup")
+let topBtn = document.querySelector(".top-btn");
 
- $(window).scroll(function(){
-    if ($(this).scrollTop() > 100) {
-        $('.scrollup').fadeIn();
-    } else {
-        $('.scrollup').fadeOut();
-    }
-});
+// On Click, Scroll to the page's top, replace 'smooth' with 'auto' if you don't want smooth scrolling
+topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
-$('.scrollup').click(function(){
-    $("html, body").animate({ scrollTop: 0 }, 600);
-    return false;
-});
+// On scroll, Show/Hide the btn with animation
+window.onscroll = () => window.scrollY > 1000 ? topBtn.style.opacity = 1 : topBtn.style.opacity = 0
+
+
+
+
+
+//  const loadMoreBtn = document.querySelector(".scrollUp")
+
+//  $(window).scroll(function(){
+//     if ($(this).scrollTop() > 100) {
+//         $('.scrollUp').fadeIn();
+//     } else {
+//         $('.scrollUp').fadeOut();
+//     }
+// });
+
+// $('.scrollup').click(function(){
+//     $("html, body").animate({ scrollTop: 0 }, 600);
+//     return false;
+// });
+
