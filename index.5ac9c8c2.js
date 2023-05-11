18c1c51aaf3b057f639273c717f50b03ac94a4f6
Luda0703 <117203301+Luda0703@users.noreply.github.com>
2023-05-11 12:44:22 +0000

    Deploying to gh-pages from @ Luda0703/Support-Ukraine@0ef88e7739073ce5af858baa247cb2c6719971d9 🚀

---
+++ index.5ac9c8c2.js
@@ -0,0 +1,2 @@
+!function(){function e(e){localStorage.setItem("theme",e),document.documentElement.className=e}"theme-dark"===localStorage.getItem("theme")?(e("theme-dark"),document.getElementById("slider").checked=!1):(e("theme-light"),document.getElementById("slider").checked=!0)}();
+//# sourceMappingURL=index.5ac9c8c2.js.map
